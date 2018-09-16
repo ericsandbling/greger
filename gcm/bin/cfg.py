cad222f0198ad03d20f324b3cc22fb58c396d859
Eric Sandbling <34446433+ericsandbling@users.noreply.github.com>
2018-09-16 21:41:02 +0200

    Adding log level control function

--- gcm/bin/cfg.py
+++ gcm/bin/cfg.py
@@ -16,6 +16,15 @@ from os.path import isfile, join
 import ConfigParser
 import logging
 
+_logLevelStr = {
+    '0' : 'NOTSET',
+    '10' : 'DEBUG',
+    '20' : 'INFO',
+    '30' : 'WARNING',
+    '40' : 'ERROR',
+    '50' : 'CRITICAL'
+}
+
 def getLocalConfig():
     '''
     Function to retrieve local configuration parameters from file.
@@ -38,3 +47,30 @@ def getLocalConfig():
         localLog.info("Loaded configuration file from: " + str(file))
 
     return config
+
+def setLogLevel(logLevel, loggerName='root'):
+    '''
+    Update logger level.
+    '''
+    # Logging
+    localLog = logging.getLogger("root.setLogLevel")
+
+    oldLogLevel = logging.getLogger(loggerName).getEffectiveLevel()
+
+    if logLevel != oldLogLevel:
+        localLog.debug("New log level detected in settings!")
+        localLog.debug("Attempting to update log level to " + _logLevelStr[str(logLevel)] + " for \"" + loggerName + "\"...")
+
+        if str(logLevel) in _logLevelStr:
+            logging.getLogger(loggerName).setLevel(logLevel)
+            localLog.debug("Log level successfully updated!")
+        else:
+            localLog.warning("Could not determin log level! - " + str(logLevel))
+            logging.getLogger(loggerName).setLevel(logging.DEBUG)
+            localLog.debug("Setting log level to default (DEBUG).")
+
+        newLogLevel = logging.getLogger(loggerName).getEffectiveLevel()
+        localLog.info("Logger \"" + loggerName + "\" set to " + _logLevelStr[str(newLogLevel)] + "!")
+
+    else:
+        localLog.debug("Log level reviewed - No change!")
