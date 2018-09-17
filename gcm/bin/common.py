a005cb553ed539b27edf0e9c89fa38db06541257
Eric Sandbling <34446433+ericsandbling@users.noreply.github.com>
2018-09-18 00:01:35 +0200

    Renamed to and including more logging methods

--- gcm/bin/common.py
+++ gcm/bin/common.py
@@ -13,15 +13,9 @@ from os.path import isfile, join
 
 import ConfigParser
 import logging
+from logging.handlers import RotatingFileHandler
 
-_logLevelStr = {
-    '0' : 'NOTSET',
-    '10' : 'DEBUG',
-    '20' : 'INFO',
-    '30' : 'WARNING',
-    '40' : 'ERROR',
-    '50' : 'CRITICAL'
-}
+#### Configuration Methods ####
 
 def getLocalConfig():
     '''
@@ -45,6 +39,72 @@ def getLocalConfig():
         localLog.info("Loaded configuration file from: " + str(file))
 
     return config
+
+#### Logging Methods ####
+
+_logLevelStr = {
+    '0' : 'NOTSET',
+    '10' : 'DEBUG',
+    '20' : 'INFO',
+    '30' : 'WARNING',
+    '40' : 'ERROR',
+    '50' : 'CRITICAL'
+}
+
+def createLogger():
+    '''
+    Create common root logger.
+    '''
+    # Get Local Configuration Parameters
+    config = getLocalConfig()
+
+    # Locally relevant parameters
+    logPath = config.get("log", "path")
+    sysLog = config.get("log", "syslog")
+
+    logMaxBytes = config.get("log", "maxbytes")
+    logBackupCount = config.get("log", "backupcount")
+
+    # Create root logger
+    logger = logging.getLogger("root")
+
+    # Set logger default level
+    logger.setLevel(logging.DEBUG)
+    # logger.setLevel(logging.INFO)
+
+    # Create logging handlers
+    consoleHandler = logging.StreamHandler()
+    # fileHandler = logging.FileHandler(logFile, mode='w')
+    rotatingFileHandler = RotatingFileHandler(
+        logPath + sysLog,
+        mode='a',
+        maxBytes=int(logMaxBytes),
+        backupCount=int(logBackupCount),
+        encoding=None,
+        delay=0)
+
+    # Set default logging level
+    # consoleHandler.setLevel(logging.DEBUG)
+    # consoleHandler.setLevel(logging.INFO)
+
+    # Create formatter
+    fmt = '%(asctime)s %(name)s [%(levelname)s] : %(message)s'
+    formatter = logging.Formatter(fmt, datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Add formatter to logging handlers
+    consoleHandler.setFormatter(formatter)
+    # fileHandler.setFormatter(formatter)
+    rotatingFileHandler.setFormatter(formatter)
+
+    # Add handlers to logger
+    logger.addHandler(consoleHandler)
+    # logger.addHandler(fileHandler)
+    logger.addHandler(rotatingFileHandler)
+
+    logger.debug("Logger created successfully!")
+
+    return logger
+
 
 def setLogLevel(logLevel, loggerName='root'):
     '''
