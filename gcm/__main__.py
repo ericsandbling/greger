da024f1f09bd3eae4a649c9567e6dcf5bc160e95
Eric Sandbling <34446433+ericsandbling@users.noreply.github.com>
2018-09-18 00:00:50 +0200

    Moved logging to common module

--- gcm/__main__.py
+++ gcm/__main__.py
@@ -13,34 +13,15 @@ import os, sys
 import logging
 
 # Custom libraries
-from gcm import GCM
-
-# Create root logger
-logger = logging.getLogger("root")
-logger.setLevel(logging.DEBUG)
-# logger.setLevel(logging.INFO)
-
-# Create logging handlers
-consoleHandler = logging.StreamHandler()
-
-# Set default logging level
-consoleHandler.setLevel(logging.DEBUG)
-# consoleHandler.setLevel(logging.INFO)
-
-# Create formatter
-formatter = logging.Formatter('%(asctime)s %(name)s [%(levelname)s] : %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-
-# Add formatter to logging handlers
-consoleHandler.setFormatter(formatter)
-
-# Add handlers to logger
-logger.addHandler(consoleHandler)
+from gcm import GregerClientModule
+import bin.common as lib
 
 if __name__ == '__main__':
 
+    logger = lib.createLogger()
     logger.info("==== APPLICATION START ====")
     try:
-        APP = GCM()
+        APP = GregerClientModule()
     except KeyboardInterrupt:
         print 'Interrupted!'
         logger.info("Interrupted!")
